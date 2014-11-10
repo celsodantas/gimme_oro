6877d8b72dfd09a2b0ce5f0614f390d9c8014212
Celso Dantas <celsodantas@gmail.com>
2014-11-10 00:58:01 -0500

    presentation fixes

--- lib/csv_importer_rbc.rb
+++ lib/csv_importer_rbc.rb
@@ -4,16 +4,27 @@ class CSVImporterRBC
 
   def import(user, content)
     CSV.parse(content).each.with_index do |row, index|
-      next if index == 0
+      begin
+        next if index == 0
 
-      entry = Entry.new(user: user)
-      entry.description = row[4]
-      entry.entry_type = row[6].to_f > 0 ? "income" : "expense"
-      entry.amount = row[6].to_f
-      entry.date = Date.strptime(row[2], "%m/%d/%Y")
+        entry = Entry.new(user: user)
+        entry.description = row[4]
+        entry.entry_type = row[6].to_f > 0 ? "income" : "expense"
+        entry.amount = row[6].to_f
+        entry.date = Date.strptime(row[2], "%m/%d/%Y")
 
-      entry.save! if entry.unique?
+        if entry.unique?
+          entry.save!
+        else
+          Rails.logger.info "[CSVImport] Skipping #{index} as duplicate"
+        end
+      rescue Exception => e
+        Rails.logger.info "row #{index} failed to import. #{e.inspect}:"
+        Rails.logger.info row
+      end
     end
+
+    true
   end
 
 end
