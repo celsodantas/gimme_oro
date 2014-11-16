0d917bc183f277110f6de948e974c6c9b3250c16
Celso Dantas <celsodantas@gmail.com>
2014-11-15 23:12:12 -0500

    removing unecessary seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: "a@b.com", password: "12345678")
